25dd3d9722ca4f08a02a2ed2bd0d220112a94b20
Martynas Muizys <muizysmartynas@gmail.com>
2024-07-12 02:03:00 +0300

    removed nvimtree and barbar. too many bugs yikes

--- lua/martynasmuizys/remap.lua
+++ lua/martynasmuizys/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", "<cmd> NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -31,3 +31,5 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader><leader>", vim.cmd.so)
