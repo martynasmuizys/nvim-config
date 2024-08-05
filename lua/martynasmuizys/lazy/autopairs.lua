a2568e3b566f60e92797c0482bf03c6a956c5a1f
Martynas Muizys <muizysmartynas@gmail.com>
2024-08-05 16:14:12 +0300

    yoinky sploinky update

--- lua/martynasmuizys/lazy/autopairs.lua
+++ lua/martynasmuizys/lazy/autopairs.lua
@@ -1,8 +1,11 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
+    config = function()
+        require("nvim-autopairs").setup({
+            enable_afterquote = false     -- add bracket pairs after quote
+        })
+    end
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
 }
-
